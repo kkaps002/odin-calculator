6943c2f319a1fec365bcd4633df8c05e548aff97
kkaps002 <kapsimaliscostas@gmail.com>
2023-10-29 21:33:44 +0000

    refactored code, started working on sequential operations feature, issue with updating current and previous operands when trying to calculate without pressing equals more than twice

--- javascript.js
+++ javascript.js
@@ -1,81 +1,72 @@
-let operator = " ";
-let previousNumber = 0;
-let currentNumber = 0;
+//initialize all values 
+let operator = null;
+let result = null;
+let previous = null;
+let current = null; 
+let displayVal = 0;
 
 
-const currentDisplay = document.getElementById("current-display");
-const previousDisplay= document.getElementById("previous-display");
+//get elements from html
+const display = document.getElementById("display");
 const buttons = document.getElementsByTagName("button");
 
-
-for (const button of buttons) {  // Add an event listener to each button
+// add event listener to each button
+for (const button of buttons) {  
     button.addEventListener("click", () => {
-        displayText(button);
+        // first check if button pressed is a number or operator or misc 
+        if (button.classList.contains("number")){ 
+            updateOperand(button);
+        }
+        else if (button.classList.contains("operator")){
+             updateOperator(button);
+        }
+        else if (button.classList.contains("equals")){ 
+            calculateResult();
+        }
+       
     });
 }
 
-
 ///////////////////////////////////////////////////////////////////////////////
-function displayText (pressedButton){
-
-    if (pressedButton.classList.contains("operator")){
-        // operator = pressedButton.textContent; // set the operator for operate function
-        // previousNumber = operate(operator,previousNumber,currentNumber); // store the current number as b operand
-        // previousDisplay.textContent = currentNumber + operator; // display it in previous section
-        // currentNumber = 0; //reset current number to be ready to receive new value
-        // currentDisplay.textContent = "0";
-        // return;
-
-        operator = pressedButton.textContent; // set the operator for operate function
-        const result = operate(operator,previousNumber,currentNumber); // store result 
-        previousNumber = result;  
-        previousDisplay.textContent = currentNumber + operator; // display it in previous section
-        currentNumber = result;
-        currentDisplay.textContent = currentNumber;
-        return;
-
-    }
-
-    if(pressedButton.textContent === "="){ //behaves almost like other buttons without changing the operator
-        const result = operate(operator,previousNumber,currentNumber); // store result 
-        previousNumber = 0;  
-        currentNumber = result;
-        previousDisplay.textContent = previousNumber;
-        currentDisplay.textContent = currentNumber;
-        return;
-    }
-
-    if (pressedButton.textContent === "C"){
-        const string = currentNumber.toString();
-        if (string.length === 1){
-            currentNumber = 0;
-            currentDisplay.textContent = currentNumber;
-            return;
-        }
-        const edited = string.slice(0,-1);
-        currentNumber = edited;
-        currentDisplay.textContent = currentNumber;
-        return;
-    }
-
-    if (pressedButton.textContent === "AC"){
-        previousNumber =0;
-        currentNumber =0;
-        previousDisplay.textContent = previousNumber;
-        currentDisplay.textContent = currentNumber;
+function updateOperand(button) {
         
-        return;
+    if (current === null || current === displayVal || current === result) { // if curr is 0 or equal to prev bc user didn't update yet
+        current = button.textContent; 
     }
-    if (currentNumber === 0){
-        currentNumber = pressedButton.textContent;
-        currentDisplay.textContent = pressedButton.textContent;
-        return;
-    }
-    currentNumber += pressedButton.textContent; // this part is repeated in almost all cases, could be written more efficiently
-    currentDisplay.textContent += pressedButton.textContent;
-    
+    else {
+        current = current + button.textContent; //apend input to the end of current number, button text is a string so both get converted here 
+    } 
+    display.textContent = current; // display user input
+    console.log("assigned:" + current);
 }
-///////////////////////////////////////////////////////////////////////////////
+
+function updateOperator(button) {
+    //remove clicked states from all operators
+    for (const button of buttons) { 
+        button.classList.remove("clicked")
+     }
+     //add clicked state to last one cliked , this way only one can appear active at a time
+    button.classList.add("clicked");
+     
+    if (previous === null){
+        previous = 0;
+    }
+    
+    if (previous !== 0){ // if prev is not 0 calculate the result of previous 2 nums before changing the operator and update curr value 
+        calculateResult();
+
+    }
+    else if (previous === 0) {  // previous is 0 only if calculate function is called 
+        operator = button.textContent; // just set the operator to user input
+        
+        previous = current; // set current to previous and standby for other input, if the user just presses equals or operator again, we get a new result but with the same number input
+        display.textContent = current;
+        // current = null; 
+        displayVal = current;
+    } 
+    // console.log(displayVal);
+    console.log("current after setting " + operator + " operator:" + current);
+}
 
 function operate(operator, a, b) { // operator needs to be string formatted
     switch(operator) {
@@ -90,8 +81,8 @@ function operate(operator, a, b) { // operator needs to be string formatted
     }
 }
 
-// b is current 
-function add(a,b) {
+
+function add(a,b) { // b is current 
     return Number(a) + Number(b);
 }
 function subtract(a,b) {
@@ -102,4 +93,14 @@ function multiply(a,b) {
 }
 function divide (a,b) {
     return Number(a) / Number(b);
+}
+
+
+//Ideally this would only deal with the display and updating the logic would be in a different funciton
+function calculateResult() { // calculate the result of an operation and update previous and current numbers 
+    result = operate(String(operator),previous,current);
+            display.textContent = result;
+            displayVal = previous;
+            previous = 0;
+            current = result;
 }
